f784fc821c24051a92d60cd9e19e1ae94043ab31
R Sharvesh <sharvesh16082007@gmail.com>
2025-07-12 11:55:48 +0530

    Add files via upload

---
+++ Odd or Even number check.c
@@ -0,0 +1,20 @@
+//Done by Sharvesh on 12.07.2025
+
+#include <stdio.h>
+
+void main()
+{
+    int num;
+    printf("Enter the Number:");
+    scanf("%d",&num);
+    
+    if(num%2==0)
+    {
+     printf("This Number is a Even Number");
+    }
+    else
+    {
+        printf("This Number is a Odd Number");
+    }
+    
+}
